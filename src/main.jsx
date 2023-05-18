4fff23bbf0f2f1c087181a3ff0c2c56972d18c77
Vishist Singh Solanki <vishistsingh16gh@gmail.com>
2023-05-19 01:39:32 +0530

    ready to deploy2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import disableReactDevTools from '@fvilers/disable-react-devtools'
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+//if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
